942a7cfcd05b7e84a6e92f93b8c38e5af11e65cc
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2022-04-30 11:40:23 -0300

    feat: Fixed header colors

--- src/components/Menu/Menu.styled.ts
+++ src/components/Menu/Menu.styled.ts
@@ -3,17 +3,21 @@ import styled from "styled-components";
 export const Menu = styled.ul`
   display: flex;
   list-style: none;
+  margin-top: 0;
+  font-size: 24px;
 
   li {
+    display: flex;
     padding: 0;
     margin: 0;
 
     a {
       padding: 8px 10px;
+      margin: 0px 2px;
       text-decoration: none;
-      color: #222;
+      color: inherit;
 
-      :hover {
+      &:hover {
         background-color: rgba(125, 125, 125, 0.2);
       }
     }
--- src/components/WebsiteHeader/WebsiteHeader.styled.ts
+++ src/components/WebsiteHeader/WebsiteHeader.styled.ts
@@ -4,19 +4,22 @@ export const WebsiteHeader = styled.header`
   display: flex;
   flex-direction: column;
   align-items: center;
+  color: white;
+  background-color: #2d3436;
 
   div.centerContainer {
     display: flex;
     flex-direction: row;
+    align-items: center;
     justify-content: space-between;
     max-width: 1200px;
     width: 100%;
-    padding: 10px;
 
     h1 {
       font-family: "Source Code Pro", monospace;
       font-weight: lighter;
       text-transform: uppercase;
+      font-size: 26px;
     }
   }
 `;
